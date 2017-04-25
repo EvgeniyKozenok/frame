e940ef453e39c20a85b7e8bb2c8db6ff493dab12
Evgeniy Kozenok <kozenokevgeniy@gmail.com>
2017-04-25 23:08:52 +0300

    feat(rendering): Rendering views improvements

--- src/Views/error/500.html.php
+++ src/Views/error/500.html.php
@@ -5,7 +5,7 @@
 <body>
 <div class="content">
     <h2>Opps... something went wrong.</h2>
-    <p style="color:#FF0000;">{{$message}}</p>
+    <p style="color:#FF0000;">{{message}}</p>
     <p>We strongly recommend you to contact your system administrator to solve it.</p>
 </div>
 </body>
